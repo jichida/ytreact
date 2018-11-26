5b391d19a1cd5285f79f09f4ec383a2483a33695
wangxiaoqing <xiaoqingwang@126.com>
2018-11-26 16:16:22 +0800

    ===性能数据===

--- appp/src/components/Performance/index.js
+++ appp/src/components/Performance/index.js
@@ -4,94 +4,7 @@ import { Flex, Accordion, WhiteSpace } from 'antd-mobile';
 
 import './index.less';
 
-const data = [
-    {
-        title: "平均电流@600",
-        sub: [
-            {
-                title: '平均',
-                content: '320',
-            },
-            {
-                title: '最小',
-                content: '320',
-            },
-            {
-                title: '最大',
-                content: '320',
-            },
-        ],
-    },
-    {
-        title: "平均电流@300",
-        sub: [
-            {
-                title: '平均',
-                content: '320',
-            },
-            {
-                title: '最小',
-                content: '320',
-            },
-            {
-                title: '最大',
-                content: '320',
-            },
-        ],
-    },
-    {
-        title: "平均CUT@600",
-        sub: [
-            {
-                title: '平均',
-                content: '320',
-            },
-            {
-                title: '最小',
-                content: '320',
-            },
-            {
-                title: '最大',
-                content: '320',
-            },
-        ],
-    },
-    {
-        title: "平均CUT@300",
-        sub: [
-            {
-                title: '平均',
-                content: '320',
-            },
-            {
-                title: '最小',
-                content: '320',
-            },
-            {
-                title: '最大',
-                content: '320',
-            },
-        ],
-    },
-    {
-        title: "净水率",
-        sub: [
-            {
-                title: '平均',
-                content: '320',
-            },
-            {
-                title: '最小',
-                content: '320',
-            },
-            {
-                title: '最大',
-                content: '320',
-            },
-        ],
-    }
-]
- 
+
 
 class Performance extends PureComponent{
 
@@ -122,7 +35,115 @@ class Performance extends PureComponent{
 
 
     render () {
-        return (
+          const devicedata = {
+            averagecurrent_600:300,//平均电流@600	600电导率时的电流:mA	1 word
+            averagecurrent_300:200,//300电导率时的电流:mA	1 word
+            averagecut_600:350,//16	平均cut@600	600电导率时的cut	1 word
+            averagecut_300:150,// 17	平均cut@300	300电导率时的cut	1 word
+            waterpurificationrate:90,//18	净水率	回收率  日用水量/(日用水量+日废水量)	1 byte
+
+            max_averagecurrent_600:120,//平均电流@600	600电导率时的电流:mA	1 word
+            max_averagecurrent_300:100,//300电导率时的电流:mA	1 word
+            max_averagecut_600:170,//16	平均cut@600	600电导率时的cut	1 word
+            max_averagecut_300:70,// 17	平均cut@300	300电导率时的cut	1 word
+            max_waterpurificationrate:19,//18	净水率	回收率  日用水量/(日用水量+日废水量)	1 byte
+
+            min_averagecurrent_600:6,//平均电流@600	600电导率时的电流:mA	1 word
+            min_averagecurrent_300:5,//300电导率时的电流:mA	1 word
+            min_averagecut_600:4,//16	平均cut@600	600电导率时的cut	1 word
+            min_averagecut_300:3,// 17	平均cut@300	300电导率时的cut	1 word
+            min_waterpurificationrate:2,//18	净水率	回收率  日用水量/(日用水量+日废水量)	1 byte
+          };
+
+
+          const data = [
+              {
+                  title: "平均电流@600",
+                  sub: [
+                      {
+                          title: '平均',
+                          content: `${devicedata.averagecurrent_600}`,
+                      },
+                      {
+                          title: '最小',
+                          content:  `${devicedata.min_averagecurrent_600}`,
+                      },
+                      {
+                          title: '最大',
+                          content:  `${devicedata.max_averagecurrent_600}`,
+                      },
+                  ],
+              },
+              {
+                  title: "平均电流@300",
+                  sub: [
+                      {
+                          title: '平均',
+                          content: `${devicedata.averagecurrent_300}`,
+                      },
+                      {
+                          title: '最小',
+                          content: `${devicedata.min_averagecurrent_300}`,
+                      },
+                      {
+                          title: '最大',
+                          content: `${devicedata.max_averagecurrent_300}`,
+                      },
+                  ],
+              },
+              {
+                  title: "平均CUT@600",
+                  sub: [
+                    {
+                        title: '平均',
+                        content: `${devicedata.averagecut_600}`,
+                    },
+                    {
+                        title: '最小',
+                        content:  `${devicedata.min_averagecut_600}`,
+                    },
+                    {
+                        title: '最大',
+                        content:  `${devicedata.max_averagecut_600}`,
+                    },
+                  ],
+              },
+              {
+                  title: "平均CUT@300",
+                  sub: [
+                    {
+                        title: '平均',
+                        content: `${devicedata.averagecut_300}`,
+                    },
+                    {
+                        title: '最小',
+                        content:  `${devicedata.min_averagecut_300}`,
+                    },
+                    {
+                        title: '最大',
+                        content:  `${devicedata.max_averagecut_300}`,
+                    },
+                  ],
+              },
+              {
+                  title: "净水率",
+                  sub: [
+                    {
+                        title: '平均',
+                        content: `${devicedata.waterpurificationrate}`,
+                    },
+                    {
+                        title: '最小',
+                        content:  `${devicedata.min_waterpurificationrate}`,
+                    },
+                    {
+                        title: '最大',
+                        content:  `${devicedata.max_waterpurificationrate}`,
+                    },
+                  ],
+              }
+          ]
+          return (
             <div className="fh_container">
                 <div className="fp_container">
                     <div className="panel">

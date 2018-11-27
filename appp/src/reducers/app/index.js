d14bfcc799a1e04f4d216cdea4856d0c91f21918
wangxiaoqing <xiaoqingwang@126.com>
2018-11-27 14:04:16 +0800

    —语言选择测试—

--- appp/src/reducers/app/index.js
+++ appp/src/reducers/app/index.js
@@ -2,7 +2,8 @@ import { createReducer } from 'redux-act';
 import {
     getsystemconfig_result,
     ui_main_selindex,
-    ui_home_selindex
+    ui_home_selindex,
+    ui_set_language
 } from '../../actions/index.js';
 // import moment from 'moment';
 
@@ -19,6 +20,9 @@ const initial = {
 const app = createReducer({
     [getsystemconfig_result]: (state, payload) => {
         return { ...state, ...payload };
+    },
+    [ui_set_language]: (state, payload) => {
+        return { ...state, locale:payload };
     },
     [ui_main_selindex]: (state, payload) => {
         return { ...state, maintabindex:payload };

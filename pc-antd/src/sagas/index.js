2bc07c574b0d85fa3002944d66fa103299129318
wangxiaoqing <xiaoqingwang@126.com>
2018-12-11 10:58:50 +0800

    ====fixed====

--- pc-antd/src/sagas/index.js
+++ pc-antd/src/sagas/index.js
@@ -2,7 +2,7 @@ import { fork } from 'redux-saga/effects';
 import {createsagacallbackflow} from './pagination';
 import {socketflow} from './ws/socketflow';
 import {wsflow} from './ws/api.ws';
-import {wififlow} from './wifi/index';
+// import {wififlow} from './wifi/index';
 // import {restfulapiflow} from './restful';
 import {uiflow} from './ui';
 import {deviceflow} from './device';
@@ -13,7 +13,7 @@ export default function* rootSaga() {
   yield fork(createsagacallbackflow);
   yield fork(socketflow);
   yield fork(wsflow);
-  yield fork(wififlow);
+  // yield fork(wififlow);
   yield fork(uiflow);
   yield fork(deviceflow);
   // yield fork(restfulapiflow);

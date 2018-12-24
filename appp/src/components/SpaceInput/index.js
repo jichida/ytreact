5b94483e870bd477742d833c2e078def83ef7167
yangweike <yangweike@hotmail.com>
2018-12-24 22:09:00 +0800

    multi input

--- appp/src/components/SpaceInput/index.js
+++ appp/src/components/SpaceInput/index.js
@@ -5,8 +5,7 @@ import './index.less'
 class SpaceInput extends React.Component {
 
     handleChange = (index,e)=>{
-        let newValue = this.props.value;
-        console.log(this.props.value)
+        let newValue = { ...this.props.value };
         newValue[index] = e.target.value;
         this.props.onChange(newValue);
     }
@@ -17,15 +16,15 @@ class SpaceInput extends React.Component {
             <div className="space-input-container">
                 <div className="space-input-item">
                     <FormattedMessage id="install.space.length" />
-                    : <input name="length" value={value.length} onChange={(e)=>{this.handleChange('length',e)}} />
+                    : <input name="length" value={value.length||''} onChange={(e)=>{this.handleChange('length',e)}} />
                 </div>
                 <div className="space-input-item">
                     <FormattedMessage id="install.space.width" />
-                    : <input name="length" value={value.width} onChange={(e)=>{this.handleChange('width',e)}} />
+                    : <input name="length" value={value.width||''} onChange={(e)=>{this.handleChange('width',e)}} />
                 </div>
                 <div className="space-input-item">
                     <FormattedMessage id="install.space.height" />
-                    : <input name="length" value={value.height} onChange={(e)=>{this.handleChange('height',e)}} />
+                    : <input name="length" value={value.height||''} onChange={(e)=>{this.handleChange('height',e)}} />
                 </div>
             </div>
         )

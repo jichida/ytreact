c0ff6c8fe7461e24ad440102817a38d028a6c54b
wangxiaoqing <xiaoqingwang@126.com>
2019-01-19 20:13:15 +0800

    ===

--- app/src/env/config.dev.js
+++ app/src/env/config.dev.js
@@ -11,7 +11,7 @@ let config = {
     serverurl:`${serverurl}`,
     wspath:`${wspath}`,
     requesttimeout:5000,
-    appversion:'1.2.12(build0118)',
+    appversion:'1.2.12(build0119)',
     sendlocationinterval:20000,
     softmode:'app'
 };

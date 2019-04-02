fb3b996ab5dccf72ed2edb3a1f5735dd1686bb1b
wangxiaoqing <xiaoqingwang@126.com>
2019-04-02 09:15:47 +0800

    1、主页左上角加语言选择 2、英语版本微调 3、断开连接图标改为暗色 4、主页滤芯页面显示 5、文字修改（正常改为状况，净水率修改）

--- appp/src/env/config.prod.js
+++ appp/src/env/config.prod.js
@@ -11,7 +11,7 @@ let config = {
     serverurl:`${serverurl}`,
     wspath:`${wspath}`,
     requesttimeout:5000,
-    appversion:'1.4.2(build0322)',
+    appversion:'1.4.3(build0402)',
     sendlocationinterval:20000,
     softmode:'appp'
 };

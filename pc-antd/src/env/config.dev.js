83344a8f8d0527e2464606e250255e8adf6298dd
wangxiaoqing <xiaoqingwang@126.com>
2019-04-29 21:11:17 +0800

    统一升级版本号

--- pc-antd/src/env/config.dev.js
+++ pc-antd/src/env/config.dev.js
@@ -12,7 +12,7 @@ const config = {
     serverurl:`${serverurl}`,
     wspath:`${wspath}`,
     requesttimeout:5000,
-    appversion:'1.3.1(build0424)',
+    appversion:'1.3.2(build0429)',
     sendlocationinterval:20000,
     softmode:'pc'
 };

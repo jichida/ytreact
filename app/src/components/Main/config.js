98197fb2b502f3e34cd5818d4fd83928603b7f5b
wangxiaoqing <xiaoqingwang@126.com>
2019-04-30 08:51:44 +0800

    config重新设置

--- app/src/components/Main/config.js
+++ app/src/components/Main/config.js
@@ -1,73 +1,73 @@
-﻿const config = {
+const config = {
     'filterelements_modlife_leftday':{
         value: 720,
         fullPercent: 95,
-        warringPercent: 1,
+        warringPercent: 10,
     },
     'filterelements_modlife_leftvol':{
         value:100,
         fullPercent: 95,
-        warringPercent: 1,
+        warringPercent: 10,
     },
     'filterelements_prefilter1_leftday':{
         value: 90,
         fullPercent: 95,
-        warringPercent: 1,
+        warringPercent: 10,
     },
     'filterelements_prefilter1_leftvol':{
         value:100,
         fullPercent: 95,
-        warringPercent: 1,
+        warringPercent: 10,
     },
     'filterelements_prefilter2_leftday':{
         value: 180,
         fullPercent: 95,
-        warringPercent: 1,
+        warringPercent: 10,
     },
     'filterelements_prefilter2_leftvol':{
         value:100,
         fullPercent: 95,
-        warringPercent: 1,
+        warringPercent: 10,
     },
     'filterelements_prefilter3_leftday':{
         value: 180,
         fullPercent: 95,
-        warringPercent: 1,
+        warringPercent: 10,
     },
     'filterelements_prefilter3_leftvol':{
         value:100,
         fullPercent: 95,
-        warringPercent: 1,
+        warringPercent: 10,
     },
     'filterelements_posfilter1_leftday':{
-        value: 90,
+        value: 190,
         fullPercent: 95,
-        warringPercent: 1,
+        warringPercent: 10,
     },
     'filterelements_posfilter1_leftvol':{
         value:100,
         fullPercent: 95,
-        warringPercent: 1,
+        warringPercent: 10,
     },
     'filterelements_posfilter2_leftday':{
         value: 90,
         fullPercent: 95,
-        warringPercent: 1,
+        warringPercent: 10,
     },
     'filterelements_posfilter2_leftvol':{
         value:100,
         fullPercent: 95,
-        warringPercent: 1,
+        warringPercent: 10,
     },
     'filterelements_posfilter3_leftday':{
-        value: 90,
+        value: 190,
         fullPercent: 95,
-        warringPercent: 1,
+        warringPercent: 10,
     },
     'filterelements_posfilter3_leftvol':{
         value:100,
         fullPercent: 95,
-        warringPercent: 1,
+        warringPercent: 10,
     }
 }
 

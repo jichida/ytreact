9a419b543902529a37f7e4aa83970cad13742101
yangweike <yangweike@hotmail.com>
2019-05-08 11:09:38 +0800

    滤芯

---
+++ appp/src/components/EquipmentList/config.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { FormattedMessage } from 'react-intl'
+
+export const prev0Options = [
+    {
+        label: <FormattedMessage key={0} id="setting.system.PP" values={{value: '30'}} />,
+        value: '30'
+    },
+    {
+        label: <FormattedMessage key={1} id="setting.system.PP" values={{value: '60'}} />,
+        value: '60'
+    },
+    {
+        label: <FormattedMessage key={2} id="setting.system.PP" values={{value: '90'}} />,
+        value: '90'
+    }
+]
+
+export const prev1Options = [
+    {
+        label: <FormattedMessage key={0} id="setting.system.carbon" values={{value: '30'}} />,
+        value: '30'
+    },
+    {
+        label: <FormattedMessage key={1} id="setting.system.carbon" values={{value: '60'}} />,
+        value: '60'
+    },
+]
+
+export const prev2Options = [
+    {
+        label: <FormattedMessage key={0} id="setting.system.TAC" values={{value: '180'}} />,
+        value: '180'
+    },
+    {
+        label: <FormattedMessage key={1} id="setting.system.TAC" values={{value: '360'}} />,
+        value: '360'
+    }
+]
+
+export const post0Options = [
+    {
+        label: <FormattedMessage key={0} id="setting.system.LED" values={{value: '180'}} />,
+        value: '180'
+    },
+    {
+        label: <FormattedMessage key={1} id="setting.system.LED" values={{value: '360'}} />,
+        value: '360'
+    },
+    {
+        label: <FormattedMessage key={2} id="setting.system.LED" values={{value: '540'}} />,
+        value: '540'
+    }
+]
+
+export const post1Options = [
+    {
+        label: <FormattedMessage key={0} id="setting.system.AFC" values={{value: '180'}} />,
+        value: '180'
+    }
+]

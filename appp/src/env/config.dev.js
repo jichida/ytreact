c67ae48132d25c81a01b221fcca84a3f55ca2358
wangxiaoqing <xiaoqingwang@126.com>
2019-05-09 15:39:09 +0800

    升级版本号 1.3.4

--- appp/src/env/config.dev.js
+++ appp/src/env/config.dev.js
@@ -11,7 +11,7 @@ let config = {
     serverurl:`${serverurl}`,
     wspath:`${wspath}`,
     requesttimeout:5000,
-    appversion:'1.3.3(build0519)',
+    appversion:'1.3.4(build0509)',
     sendlocationinterval:20000,
     softmode:'appp'
 };

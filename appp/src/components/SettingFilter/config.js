85f54c4269594767ce977efea6055d9e5d5231f8
yangweike <yangweike@hotmail.com>
2019-06-10 21:48:43 +0800

    滤镜选择

--- appp/src/components/SettingFilter/config.js
+++ appp/src/components/SettingFilter/config.js
@@ -14,6 +14,10 @@ export const prev0Options = [
     {
         label: <FormattedMessage key={2} id="setting.system.PP" values={{value: '90'}} />,
         value: '90'
+    },
+    {
+      label: <FormattedMessage key={3} id="form.equip.nonefilter" defaultMessage="无" />,
+      value: '0'
     }
 ]
 
@@ -26,6 +30,10 @@ export const prev1Options = [
         label: <FormattedMessage key={1} id="setting.system.carbon" values={{value: '60'}} />,
         value: '60'
     },
+    {
+      label: <FormattedMessage key={2} id="form.equip.nonefilter" defaultMessage="无" />,
+      value: '0'
+    }
 ]
 
 export const prev2Options = [
@@ -36,6 +44,10 @@ export const prev2Options = [
     {
         label: <FormattedMessage key={1} id="setting.system.TAC" values={{value: '360'}} />,
         value: '360'
+    },
+    {
+      label: <FormattedMessage key={2} id="form.equip.nonefilter" defaultMessage="无" />,
+      value: '0'
     }
 ]
 
@@ -51,6 +63,10 @@ export const post0Options = [
     {
         label: <FormattedMessage key={2} id="setting.system.LED" values={{value: '540'}} />,
         value: '540'
+    },
+    {
+      label: <FormattedMessage key={3} id="form.equip.nonefilter" defaultMessage="无" />,
+      value: '0'
     }
 ]
 
@@ -58,6 +74,10 @@ export const post1Options = [
     {
         label: <FormattedMessage key={0} id="setting.system.AFC" values={{value: '180'}} />,
         value: '180'
+    },
+    {
+      label: <FormattedMessage key={1} id="form.equip.nonefilter" defaultMessage="无" />,
+      value: '0'
     }
 ]
 

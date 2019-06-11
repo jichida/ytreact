b47ba7f0c5ecc1616a00541900dbac63c26ded91
wangxiaoqing <xiaoqingwang@126.com>
2019-06-11 16:54:58 +0800

    更新版本号

--- appp/src/env/config.prod.js
+++ appp/src/env/config.prod.js
@@ -11,7 +11,7 @@ let config = {
     serverurl:`${serverurl}`,
     wspath:`${wspath}`,
     requesttimeout:5000,
-    appversion:'1.4.0(build0607)',
+    appversion:'1.4.1(build0611)',
     sendlocationinterval:20000,
     softmode:'appp'
 };

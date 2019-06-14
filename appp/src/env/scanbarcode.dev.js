7b1f6793d317b9594fc55f3893a1b436f1339cb9
yangweike <yangweike@hotmail.com>
2019-06-14 17:31:42 +0800

    设备列表

--- appp/src/env/scanbarcode.dev.js
+++ appp/src/env/scanbarcode.dev.js
@@ -2,7 +2,8 @@ export const scanbarcode=(fncallback)=>{
     window.setTimeout(()=>{
       fncallback({
         "code": 0,
-        "data": "GHCA0488",
+        // "data": "GHCA0488",
+        "data": "GHDD1201"
       })
     },0);
 };

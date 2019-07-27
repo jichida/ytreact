e4173571b3151602e5ad2949c01ebc5606c07cfc
yangweike <yangweike@hotmail.com>
2019-07-27 15:51:14 +0800

    语言选择

--- app/src/components/Home/index.js
+++ app/src/components/Home/index.js
@@ -3,23 +3,70 @@ import { connect } from 'react-redux';
 import { NavBar } from 'antd-mobile';
 import { withRouter } from 'react-router-dom';
 import { injectIntl } from 'react-intl'
+import { Popover } from 'antd-mobile'
 
 import Main from '../Main';
 
 import './index.less';
 import '../../assets/wlimg.png';
-import {ui_home_selindex} from '../../actions';
+import {ui_home_selindex, ui_set_language} from '../../actions';
+
+const Item = Popover.Item
+
+const lanString = {
+    'en': 'EN',
+    'zh-cn': '简',
+    'zh-tw': '繁'
+}
 
 class Home extends PureComponent{
+    state = {
+        visible: false,
+        language: 'zh-cn'
+    }
+
+    handleVisibleChange = (visible) => {
+        this.setState({
+          visible,
+        });
+    }
+
+    onSelect = (opt) => {
+        console.log(opt.props.value);
+        this.props.dispatch(ui_set_language(opt.props.value))
+        this.setState({
+            visible: false
+        });
+    }
 
     render () {
-        const { intl: { formatMessage } } = this.props;
-
+        const { intl: { formatMessage }, locale } = this.props;
 
         return (
             <div className="home">
                 <NavBar
                     className="nav"
+                    leftContent={
+                        <Popover mask
+                          visible={this.state.visible}
+                          placement = "bottomLeft"
+                          overlay={[
+                            (<Item key="4" value="zh-cn" data-seed="logId">中文简体</Item>),
+                            (<Item key="5" value="zh-tw">中文繁體</Item>),
+                            (<Item key="6" value="en">English</Item>),
+                          ]}
+                          align={{
+                            overflow: { adjustY: 30, adjustX: 30 },
+                            offset: [10, 30],
+                          }}
+                          onVisibleChange={this.handleVisibleChange}
+                          onSelect={this.onSelect}
+                        >
+                          <div>
+                            {lanString[locale]}
+                          </div>
+                        </Popover>
+                      }
                     // rightContent={[<div key="0" className="nav-wifi-icon"/>]}
                 >
                 { formatMessage({id: "home"})}
@@ -32,8 +79,11 @@ class Home extends PureComponent{
         )
     }
 }
-const mapStateToProps =  ({app:{hometabindex}}) =>{
-  return {curtab:hometabindex};
+const mapStateToProps =  ({app:{hometabindex, locale}}) =>{
+  return {
+      curtab:hometabindex,
+      locale
+    };
 };
 Home = connect(mapStateToProps)(Home);
 export default withRouter(injectIntl(Home));

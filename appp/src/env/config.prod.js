759b793d2bccbf95cfd988707159eb21a28cb0d3
wangxiaoqing <xiaoqingwang@126.com>
2019-08-09 22:20:08 +0800

    硬件调试成功

--- appp/src/env/config.prod.js
+++ appp/src/env/config.prod.js
@@ -11,7 +11,7 @@ let config = {
     serverurl:`${serverurl}`,
     wspath:`${wspath}`,
     requesttimeout:5000,
-    appversion:'1.0.0(build080922)',
+    appversion:'1.0.0(build080923)',
     sendlocationinterval:20000,
     softmode:'appp'
 };

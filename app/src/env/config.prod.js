b19ee05b075117a729d948ccc1de69d35f1a88b5
wangxiaoqing <xiaoqingwang@126.com>
2019-09-07 12:38:56 +0800

    设备密码

--- app/src/env/config.prod.js
+++ app/src/env/config.prod.js
@@ -11,7 +11,7 @@ let config = {
     serverurl:`${serverurl}`,
     wspath:`${wspath}`,
     requesttimeout:5000,
-    appversion:'1.0.0(build0825)',
+    appversion:'1.0.0(build0907)',
     sendlocationinterval:20000,
     softmode:'app'
 };
